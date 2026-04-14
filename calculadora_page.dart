d70ecc7572f6f20bccea27e2bf82a1f68db2d635
Pedro Paulo <92110338+PedroPaulo00@users.noreply.github.com>
2026-04-14 20:12:59 -0300

    Implement calculator logic and UI updates
    Added functionality for number input, operations, and calculation logic.

--- calculadora_page.dart
+++ calculadora_page.dart
@@ -1,3 +1,4 @@
+import 'package:calculadora_pet/pages/operation_type.dart';
 import 'package:calculadora_pet/widgets/buttom_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,119 @@ class CalculadoraPage extends StatefulWidget {
 }
 
 class _CalculadoraPageState extends State<CalculadoraPage> {
+  late String displaynumber;
+
+  void initState(){
+    displaynumber='0';
+    super.initState();
+  }
+
+  void setOperationType(OperationTypeEnum newType){
+    setState(() {
+      displaynumber += newType.symbol;
+    });
+  }
+
+  void clearCalculator(){
+    setState((){
+      displaynumber = '0';
+    });
+  }
+
+  void appendNumber(String stringNumber){
+    setState(() {
+      if(displaynumber == '0'){
+        displaynumber = stringNumber;
+      }else{
+        displaynumber+= stringNumber;
+      }
+    });
+  }
+
+  void removeNumber(){
+    if(displaynumber.length <=1){
+      displaynumber = '0';
+    }
+    else{
+      displaynumber = displaynumber.substring(0, displaynumber.length-1);
+    }
+  }
+
+  List<double> parseNumbers(String expression){
+    RegExp regExp = RegExp(r'[0-9]+\.?[0-9]*');
+
+    var matches = regExp.allMatches(expression);
+
+    List<double> numbers = [];
+
+    for(var match in matches){
+      String numberText = match.group(0)!;
+      numbers.add(double.parse(numberText));
+    }
+
+    return numbers;
+  }
+
+  List<OperationTypeEnum> getOperators(String expression){
+    final expression1 = expression.characters.where(
+      (x) => OperationTypeEnum.values.any((op)=> op.symbol ==x),
+    );
+
+    return expression1.map((x)=> OperationTypeEnum.values.firstWhere((op)=> op.symbol ==x)).toList();
+  }
+
+  void calculate(){
+    String expression = displaynumber.replaceAll(',', '.');
+
+    List<double> numbers = parseNumbers(expression);
+    List<OperationTypeEnum> operations = getOperators(expression);
+
+    resolvePriorityOperations(numbers, operations);
+    final result = resolveAdditionAndSubtraction(numbers, operations);
+    setState(() {
+      displaynumber = result.toString().replaceAll('.', ',');
+    });
+  }
+
+  void resolvePriorityOperations(List<double> numbers, List<OperationTypeEnum> operators,){
+    int index = 0;
+
+    while(index < operators.length){
+      if(operators[index] == OperationTypeEnum.multiplication){
+        numbers[index] = numbers[index] * numbers[index+1];
+        numbers.removeAt(index+1);
+        operators.removeAt(index);
+      }else if(operators[index] == OperationTypeEnum.division){
+        numbers[index] = numbers[index]/numbers[index+1];
+        numbers.removeAt(index+1);
+        operators.removeAt(index);
+      }else{
+        index++;
+      }
+    }
+  }
+
+  double resolveAdditionAndSubtraction(
+    List<double> numbers,
+    List<OperationTypeEnum> operators,
+  ){
+    int index = 0;
+
+    while(index<operators.length){
+      if(operators[index] == OperationTypeEnum.addition){
+        numbers[index] = numbers[index] + numbers[index+1];
+        numbers.removeAt(index+1);
+        operators.removeAt(index);
+      }else if(operators[index] == OperationTypeEnum.subtraction){
+        numbers[index] = numbers[index] - numbers[index+1];
+        numbers.removeAt(index+1);
+        operators.removeAt(index);
+      }
+    }
+
+    return numbers[0];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +139,7 @@ class _CalculadoraPageState extends State<CalculadoraPage> {
             child: Align(
               alignment: Alignment.bottomRight,
               child: Text(
-                "0",
+                displaynumber,
                 style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
               ),
             ),
@@ -35,36 +149,72 @@ class _CalculadoraPageState extends State<CalculadoraPage> {
               children: [
                 Row(
                   children: [
-                    ButtomWidget(text: "C", onPressed: (){}, color: Colors.red,),
-                    ButtomWidget(text: "\u232b", onPressed: (){}, color: Colors.orange),
-                    ButtomWidget(text: '÷', onPressed: (){}, color: Colors.blue, textColor: Colors.white),
+                    ButtomWidget(text: "C", onPressed: (){
+                      clearCalculator();
+                    }, color: Colors.red,),
+                    ButtomWidget(text: "\u232b", onPressed: (){
+                      removeNumber();
+                    }, color: Colors.orange),
+                    ButtomWidget(text: '÷', onPressed: (){
+                      setOperationType(OperationTypeEnum.division);
+                    }, color: Colors.blue, textColor: Colors.white),
                   ],
                 ),
                 Row(children: [
-                  ButtomWidget(text: '7', onPressed: (){}),
-                  ButtomWidget(text: '8', onPressed: (){}),
-                  ButtomWidget(text: '9', onPressed: (){}),
-                  ButtomWidget(text: "X", onPressed: (){}, color: Colors.blue, textColor: Colors.white),
+                  ButtomWidget(text: '7', onPressed: (){
+                    appendNumber('7');
+                  }),
+                  ButtomWidget(text: '8', onPressed: (){
+                    appendNumber('8');
+                  }),
+                  ButtomWidget(text: '9', onPressed: (){
+                    appendNumber('9');
+                  }),
+                  ButtomWidget(text: "X", onPressed: (){
+                    setOperationType(OperationTypeEnum.multiplication);
+                  }, color: Colors.blue, textColor: Colors.white),
                 ],
                 ),
                 Row(children: [
-                  ButtomWidget(text: '4', onPressed: (){}),
-                  ButtomWidget(text: '5', onPressed: (){}),
-                  ButtomWidget(text: '6', onPressed: (){}),
-                  ButtomWidget(text: "-", onPressed: (){}, color: Colors.blue, textColor: Colors.white),
+                  ButtomWidget(text: '4', onPressed: (){
+                    appendNumber('4');
+                  }),
+                  ButtomWidget(text: '5', onPressed: (){
+                    appendNumber('5');
+                  }),
+                  ButtomWidget(text: '6', onPressed: (){
+                    appendNumber('6');
+                  }),
+                  ButtomWidget(text: "-", onPressed: (){
+                    setOperationType(OperationTypeEnum.subtraction);
+                  }, color: Colors.blue, textColor: Colors.white),
                 ],
                 ),
                 Row(children: [
-                  ButtomWidget(text: '1', onPressed: (){}),
-                  ButtomWidget(text: '2', onPressed: (){}),
-                  ButtomWidget(text: '3', onPressed: (){}),
-                  ButtomWidget(text: "+", onPressed: (){}, color: Colors.blue, textColor: Colors.white),
+                  ButtomWidget(text: '1', onPressed: (){
+                    appendNumber('1');
+                  }),
+                  ButtomWidget(text: '2', onPressed: (){
+                    appendNumber('2');
+                  }),
+                  ButtomWidget(text: '3', onPressed: (){
+                    appendNumber('3');
+                  }),
+                  ButtomWidget(text: "+", onPressed: (){
+                    setOperationType(OperationTypeEnum.addition);
+                  }, color: Colors.blue, textColor: Colors.white),
                 ],
                 ),
                 Row(children: [
-                  ButtomWidget(text: '0', onPressed: (){}),
-                  ButtomWidget(text: ',', onPressed: (){}),
-                  ButtomWidget(text: "=", onPressed: (){}, color: Colors.green),
+                  ButtomWidget(text: '0', onPressed: (){
+                    appendNumber('0');
+                  }),
+                  ButtomWidget(text: ',', onPressed: (){
+                    appendNumber(',');
+                  }),
+                  ButtomWidget(text: "=", onPressed: (){
+                    calculate();
+                  }, color: Colors.green),
                 ],)
               ],
             )
